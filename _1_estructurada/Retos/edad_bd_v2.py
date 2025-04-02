bbb0bef2043d46488badb951106cdf7adfd80317
fany alondra juarez rodriguez <fanyj343@gmail.com>
2025-04-02 17:44:55 -0600

    colores

---
+++ _1_estructurada/Retos/edad_bd_v2.py
@@ -0,0 +1,55 @@
+import json
+import sys
+
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+RESET = "\033[0m"
+
+
+def extrae():
+    try:
+        archivo = open("edad_bd.json", "r", encoding="utf-8")
+        datos = json.load(archivo)
+        for persona in datos["persona"]:
+            yield (persona["id"], persona["nombre"], persona["edad"], persona["RFC"])
+
+    except FileExistsError:
+        print(RED, "¡Error!El archivo no existe ")
+    except json.JSONDecodeError:
+        print(RED, "¡Error!El archivo no contiene un JSON valido ")
+    except Exception as e:
+        print(GREEN, "Pues no se que ocurrio ", e)
+
+if __name__ == '__main__':
+    i = 1
+    for id, nombre, edad, RFC in extrae():
+        match i:
+            case 1:
+                sys.stdout.write(MAGENTA)
+            case 2:
+                sys.stdout.write(BLUE)
+            case 3:
+                sys.stdout.write(CYAN)
+            case _:
+                sys.stdout.write(RESET)
+
+        print("-" * 30)
+        print(f"id:{id} ")
+        print(f" nombre: {nombre}")
+        print(f"edad: {edad}")
+        print(f"RFC: {RFC}")
+
+        if int(edad) >= 18:
+            print("mayor de edad")
+        else:
+            print("menor de edad")
+            print("-" * 30)
+
+            sys.stdout.write(RESET)
+            i += 1
